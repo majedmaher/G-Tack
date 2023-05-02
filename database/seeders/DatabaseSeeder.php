ad0c193a323cfb5e57bb0d01b26ef66fe395662e
Ali Alqrinawi <104166368+AliAlqrinawi@users.noreply.github.com>
2023-05-02 15:05:37 +0300

    'seed'

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Vendor;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,11 +20,35 @@ class DatabaseSeeder extends Seeder
             JarsSeeder::class,
         ]);
 
-        // \App\Models\User::factory(10)->create();
+        $GOVERNORATE = \App\Models\Location::create([
+            'name' => 'Gaza',
+            'type' => 'GOVERNORATE',
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $REGION = \App\Models\Location::create([
+            'name' => 'Remal',
+            'type' => 'REGION',
+            'parent_id' => $GOVERNORATE->id,
+        ]);
+
+        for($i = 0 ; $i <= 10 ; $i++){
+        $user = \App\Models\User::create([
+                'name' => 'Test User',
+                'type' => 'VENDOR',
+                'phone' => '52314679'.$i,
+                'otp' => '1234',
+                'email' => 'test@example.com'.$i,
+                'password' => 'test@example.com'.$i,
+            ]);
+            $vendor = new Vendor();
+            $vendor->name = 'Test User';
+            $vendor->commercial_name = 'commercial_name';
+            $vendor->phone = '52314679'.$i;
+            $vendor->user_id  = $user->id;
+            $vendor->governorate_id = 1;
+            $vendor->region_id  = 2;
+            $vendor->save();
+        }
+
     }
 }
