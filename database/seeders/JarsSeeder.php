4b3839392700f165ea99ded0b00d64aeacb6993f
Ali Alqrinawi <104166368+AliAlqrinawi@users.noreply.github.com>
2023-05-02 13:34:26 +0300

    'jars'

---
+++ database/seeders/JarsSeeder.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Jar;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class JarsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            [
+                'name' => 'جره 12 كيلو',
+                'price' => '70',
+                'size' => '12',
+                'image' => 'image/image-2.png',
+                'status' => 'ACTIVE',
+            ],
+            [
+                'name' => 'جره 60 كيلو',
+                'price' => '700',
+                'size' => '12',
+                'image' => 'image/image-1.png',
+                'status' => 'ACTIVE',
+            ],
+            [
+                'name' => 'جره 12 كيلو',
+                'price' => '70',
+                'size' => '12',
+                'image' => 'image/image-3.png',
+                'status' => 'ACTIVE',
+            ],
+            [
+                'name' => 'جره 45 كيلو',
+                'price' => '400',
+                'size' => '12',
+                'image' => 'image/image-4.png',
+                'status' => 'ACTIVE',
+            ],
+        ];
+
+        foreach ($data as $key => $value) {
+            Jar::create([
+                'name' => $value['name'],
+                'price' => $value['price'],
+                'size' => $value['size'],
+                'image' => $value['image'],
+                'status' => $value['status'],
+            ]);
+        }
+    }
+}
