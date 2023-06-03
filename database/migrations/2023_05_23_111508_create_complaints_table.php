03e7daca8aba2d39c225f751ed78b378b71ae63a
Ali Alqrinawi <104166368+AliAlqrinawi@users.noreply.github.com>
2023-06-03 12:14:21 +0300

    'order'

--- database/migrations/2023_05_23_111508_create_complaints_table.php
+++ database/migrations/2023_05_23_111508_create_complaints_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('complaints', function (Blueprint $table) {
             $table->id();
             $table->enum('vendor_type' , ['GAS' , 'WATER']);
-            $table->enum('type' , ['CUSTMER' , 'VENDOR']);
+            $table->enum('type' , ['CUSTOMER' , 'VENDOR']);
             $table->foreignId('customer_id')->nullable()->constrained('customers', 'id')->cascadeOnDelete()->cascadeOnUpdate();
             $table->foreignId('vendor_id')->nullable()->constrained('vendors', 'id')->cascadeOnDelete()->cascadeOnUpdate();
             $table->foreignId('order_id')->constrained('orders', 'id')->cascadeOnDelete()->cascadeOnUpdate();
