ccfdcc3c0aca0f2ac80d25ca71209509da05b4d2
Ali Alqrinawi <104166368+AliAlqrinawi@users.noreply.github.com>
2023-06-06 15:00:31 +0300

    'seeder'

--- database/seeders/VenderSeeder.php
+++ database/seeders/VenderSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use App\Models\Vendor;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +16,71 @@ class VenderSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $data = [
+            [
+                'name' => 'هاني فرينة',
+                'commercial_name' => 'شركة فرينة لتعبئة الغاز',
+                'type' => 'VENDOR',
+                'phone' => '09781956465',
+                'otp' => '1234',
+                'email' => 'tes1t@example.com',
+                'password' => 'tes1t@example.com',
+            ],
+            [
+                'name' => 'ناريمان زقوت',
+                'commercial_name' => 'شركة زقوت لتعبئة الغاز',
+                'type' => 'VENDOR',
+                'phone' => '059456987',
+                'otp' => '1234',
+                'email' => 'nareman@example.com',
+                'password' => 'nareman@example.com',
+            ],
+            [
+                'name' => 'منى محمد',
+                'commercial_name' => 'تروبة رفح لتعبة الغاز',
+                'type' => 'VENDOR',
+                'phone' => '0564568779',
+                'otp' => '1234',
+                'email' => 'mona@example.com',
+                'password' => 'mona@example.com',
+            ],
+            [
+                'name' => 'مصعب غراب',
+                'commercial_name' => 'شركة غراب لتعبئة الغاز',
+                'type' => 'VENDOR',
+                'phone' => '059132456',
+                'otp' => '1234',
+                'email' => 'mosad@example.com',
+                'password' => 'mosad@example.com',
+            ],
+            [
+                'name' => 'شوق الحلو',
+                'commercial_name' => 'شركة الحلو لتعبئة الغاز',
+                'type' => 'VENDOR',
+                'phone' => '0978956465',
+                'otp' => '1234',
+                'email' => 'test@example.com',
+                'password' => 'test@example.com',
+            ],
+        ];
+
+        foreach ($data as $key => $value) {
+            $user = User::create([
+                'name' => $value['name'],
+                'type' => $value['type'],
+                'phone' => $value['phone'],
+                'otp' => $value['otp'],
+                'email' => $value['email'],
+                'password' => $value['password'],
+            ]);
+            Vendor::create([
+                'name' => $value['name'],
+                'commercial_name' => $value['commercial_name'],
+                'phone' => $value['phone'],
+                'user_id' => $user->id,
+                'governorate_id' => 1,
+                'region_id' => 2,
+            ]);
+        }
     }
 }
