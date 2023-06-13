01b4adf910ed8b80e4f3a97675197ba29e8f038f
Ali Alqrinawi <104166368+AliAlqrinawi@users.noreply.github.com>
2023-06-13 11:50:24 +0300

    'authPusher'

--- app/Events/OrderTracking.php
+++ app/Events/OrderTracking.php
@@ -34,7 +34,7 @@ class OrderTracking implements ShouldBroadcast
      */
     public function broadcastOn()
     {
-        return new PrivateChannel('private-order-tracking-' . $this->order->id);
+        return new Channel('private-order-tracking-' . $this->order->id);
     }
 
     public function broadcastAs()
