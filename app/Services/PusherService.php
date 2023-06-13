04985798b777d9b1dc74ca775216fabd9fe95716
Ali Alqrinawi <104166368+AliAlqrinawi@users.noreply.github.com>
2023-06-13 10:33:46 +0300

    'authPusher'

--- app/Services/PusherService.php
+++ app/Services/PusherService.php
@@ -5,6 +5,7 @@ namespace App\Services;
 use App\Events\OrderTracking;
 use App\Models\Order;
 use Illuminate\Support\Facades\Auth;
+use Pusher\Pusher;
 use Throwable;
 
 class PusherService
@@ -23,5 +24,22 @@ class PusherService
         } catch (Throwable $e) {
             throw $e;
         }
+    }
+
+    public function auth($data){
+        $pusher = new Pusher(
+            config('broadcasting.connections.pusher.key'),
+            config('broadcasting.connections.pusher.secret'),
+            config('broadcasting.connections.pusher.app_id'),
+            [
+                'cluster' => config('broadcasting.connections.pusher.options.cluster'),
+                'encrypted' => true,
+            ]
+        );
+        $channel = $data['channel_name'];
+        $socketId = $data['socket_id'];
+        // Generate the authentication response with the channel name and socket ID
+        $auth = $pusher->socket_auth($channel, $socketId);
+        return response()->json($auth);
     }
 }
