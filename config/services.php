a054a4ee98e04b6a3b96a6bf977872092edacc2d
Ali Alqrinawi <104166368+AliAlqrinawi@users.noreply.github.com>
2023-06-24 14:19:22 +0300

    ''

--- config/services.php
+++ config/services.php
@@ -31,4 +31,11 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'fcm' => [
+        'driver' => 'fcm',
+        'credentials' => env('FIREBASE_CREDENTIALS'),
+    ],
+
+
+
 ];
