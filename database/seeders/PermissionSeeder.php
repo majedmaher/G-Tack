dbfc739e5cca002a20fcbff18255704a018a8fbd
Ali Alqrinawi <104166368+AliAlqrinawi@users.noreply.github.com>
2023-07-13 10:48:15 +0300

    per

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -17,31 +17,121 @@ class PermissionSeeder extends Seeder
     {
         $data = [
             [
-                'name' => 'name',
+                'name' => 'overView_view',
             ],
             [
-                'name' => 'name',
+                'name' => 'overView_add',
             ],
             [
-                'name' => 'name',
+                'name' => 'overView_edit',
             ],
             [
-                'name' => 'name',
+                'name' => 'overView_delete',
+            ],
+
+
+            [
+                'name' => 'order_view',
             ],
             [
-                'name' => 'name',
+                'name' => 'order_add',
             ],
             [
-                'name' => 'name',
+                'name' => 'order_edit',
             ],
             [
-                'name' => 'name',
+                'name' => 'order_delete',
+            ],
+
+            [
+                'name' => 'vendor_view',
             ],
             [
-                'name' => 'name',
+                'name' => 'vendor_add',
             ],
             [
-                'name' => 'name',
+                'name' => 'vendor_edit',
+            ],
+            [
+                'name' => 'vendor_delete',
+            ],
+
+            [
+                'name' => 'customer_view',
+            ],
+            [
+                'name' => 'customer_add',
+            ],
+            [
+                'name' => 'customer_edit',
+            ],
+            [
+                'name' => 'customer_delete',
+            ],
+
+            [
+                'name' => 'map_view',
+            ],
+            [
+                'name' => 'map_add',
+            ],
+            [
+                'name' => 'map_edit',
+            ],
+            [
+                'name' => 'map_delete',
+            ],
+
+            [
+                'name' => 'reports_view',
+            ],
+            [
+                'name' => 'reports_add',
+            ],
+            [
+                'name' => 'reports_edit',
+            ],
+            [
+                'name' => 'reports_delete',
+            ],
+
+            [
+                'name' => 'user_view',
+            ],
+            [
+                'name' => 'user_add',
+            ],
+            [
+                'name' => 'user_edit',
+            ],
+            [
+                'name' => 'user_delete',
+            ],
+
+            [
+                'name' => 'complaint_view',
+            ],
+            [
+                'name' => 'complaint_add',
+            ],
+            [
+                'name' => 'complaint_edit',
+            ],
+            [
+                'name' => 'complaint_delete',
+            ],
+
+            [
+                'name' => 'settings_view',
+            ],
+            [
+                'name' => 'settings_add',
+            ],
+            [
+                'name' => 'settings_edit',
+            ],
+            [
+                'name' => 'settings_delete',
             ],
         ];
 
